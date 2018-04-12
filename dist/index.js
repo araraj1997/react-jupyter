e25827867936aff3060a21cf407bd622916356f5
eoinmurray <amonecho1@gmail.com>
2018-04-12 19:25:56 -0400

    react fix

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,29 @@
-"use strict";
+'use strict';
 
 exports.__esModule = true;
 exports.default = undefined;
 
-var _queryString = require("query-string");
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _queryString = require('query-string');
 
 var _queryString2 = _interopRequireDefault(_queryString);
 
-var _transforms = require("@nteract/transforms");
+var _transforms = require('@nteract/transforms');
 
-var _notebookPreview = require("@nteract/notebook-preview");
+var _notebookPreview = require('@nteract/notebook-preview');
 
 var _notebookPreview2 = _interopRequireDefault(_notebookPreview);
 
-var _transformDataresource = require("@nteract/transform-dataresource");
+var _transformDataresource = require('@nteract/transform-dataresource');
 
 var _transformDataresource2 = _interopRequireDefault(_transformDataresource);
 
-var _transformVega = require("@nteract/transform-vega");
+var _transformVega = require('@nteract/transform-vega');
 
-var _PlotlyTransform = require("./PlotlyTransform");
+var _PlotlyTransform = require('./PlotlyTransform');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -38,7 +42,7 @@ var _reduce = [_transformDataresource2.default, _transformVega.VegaLite1, _trans
 
 var setDisplay = function setDisplay(_class, val) {
   Array.from(document.getElementsByClassName(_class)).forEach(function (elem) {
-    return elem.setAttribute('style', "display:" + val + " !important");
+    return elem.setAttribute('style', 'display:' + val + ' !important');
   });
 };
 
@@ -97,41 +101,41 @@ var _default = function (_React$Component) {
     var display = this.state.display;
 
 
-    var style = "\n      " + (display === 'hidden' || display === 'output' ? "\n        .input-container {\n          display: none!important;\n        }\n      " : "") + "\n\n      " + (display === 'hidden' ? "\n        .nteract-display-area-stdout {\n          display: none!important;\n        }\n\n        .nteract-display-area-stderr {\n          display: none!important;\n        }\n      " : "") + "\n    ";
+    var style = '\n      ' + (display === 'hidden' || display === 'output' ? '\n        .input-container {\n          display: none!important;\n        }\n      ' : '') + '\n\n      ' + (display === 'hidden' ? '\n        .nteract-display-area-stdout {\n          display: none!important;\n        }\n\n        .nteract-display-area-stderr {\n          display: none!important;\n        }\n      ' : '') + '\n    ';
 
-    return React.createElement(
-      "div",
+    return _react2.default.createElement(
+      'div',
       null,
-      "Code:",
+      'Code:',
       '  ',
-      React.createElement(
-        "select",
+      _react2.default.createElement(
+        'select',
         { onChange: function onChange(e) {
             return _this2.onChangeDisplay(e);
           }, value: display },
-        React.createElement(
-          "option",
-          { value: "hidden", defaultValue: display === 'hidden' },
-          "Hidden"
+        _react2.default.createElement(
+          'option',
+          { value: 'hidden', defaultValue: display === 'hidden' },
+          'Hidden'
         ),
-        React.createElement(
-          "option",
-          { value: "shown", defaultValue: display === 'shown' },
-          "Shown"
+        _react2.default.createElement(
+          'option',
+          { value: 'shown', defaultValue: display === 'shown' },
+          'Shown'
         ),
-        React.createElement(
-          "option",
-          { value: "output", defaultValue: display === 'output' },
-          "Hidden, but show code outputs"
+        _react2.default.createElement(
+          'option',
+          { value: 'output', defaultValue: display === 'output' },
+          'Hidden, but show code outputs'
         )
       ),
-      React.createElement(_notebookPreview2.default, {
+      _react2.default.createElement(_notebookPreview2.default, {
         notebook: content,
         displayOrder: displayOrder,
         transforms: transforms
       }),
-      React.createElement(
-        "style",
+      _react2.default.createElement(
+        'style',
         { jsx: true, global: true },
         style
       )
@@ -139,7 +143,7 @@ var _default = function (_React$Component) {
   };
 
   return _default;
-}(React.Component);
+}(_react2.default.Component);
 
 exports.default = _default;
-module.exports = exports["default"];
+module.exports = exports['default'];
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import queryString from 'query-string'
 import {
   standardTransforms,
